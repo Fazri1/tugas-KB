5f4f67359adeec42b7a7e7b80d54f6623a965f2c
Fazri Egi <71480242+Fazri1@users.noreply.github.com>
2022-06-21 09:30:17 +0700

    Add files via upload

---
+++ knn.py
@@ -0,0 +1,118 @@
+# put the dataset directory and source code files in the same directory
+
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from imutils import paths
+import numpy as np
+import argparse
+import cv2
+import os
+
+class SimplePreprocessor:
+    def __init__(self, width, height, inter=cv2.INTER_AREA):
+        # store the target image width, height, and interpolation
+        # method used when resizing
+        self.width = width
+        self.height = height
+        self.inter = inter
+    def preprocess(self, image):
+        # resize the image to a fixed size, ignoring the aspect
+        # ratio
+        return cv2.resize(image, (self.width, self.height), interpolation=self.inter)
+
+class SimpleDatasetLoader:
+    def __init__(self, preprocessors=None):
+        # store the image preprocessor
+        self.preprocessors = preprocessors
+        # if the preprocessors are None, initialize them as an
+        # empty list
+        if self.preprocessors is None:
+            self.preprocessors = []
+    def load(self, imagePaths, verbose=-1):
+        # initialize the list of features and labels
+        data = []
+        labels = []
+        # loop over the input images
+        for (i, imagePath) in enumerate(imagePaths):
+            # load the image and extract the class label assuming
+            # that our path has the following format:
+            # /path/to/dataset/{class}/{image}.jpg
+            image = cv2.imread(imagePath)
+            label = imagePath.split(os.path.sep)[-2]
+            # check to see if our preprocessors are not None
+            if self.preprocessors is not None:
+                # loop over the preprocessors and apply each to
+                # the image
+                for p in self.preprocessors:
+                    image = p.preprocess(image)
+            # treat our processed image as a "feature vector"
+            # by updating the data list followed by the labels
+            data.append(image)
+            labels.append(label)
+            # show an update every `verbose` images
+            if verbose > 0 and i > 0 and (i + 1) % verbose == 0:
+                print("[INFO] processed {}/{}".format(i + 1, len(imagePaths)))
+        # return a tuple of the data and labels
+        return (np.array(data), np.array(labels))
+
+    def load_data(self, imagePath):
+        image = cv2.imread(imagePath)
+        if self.preprocessors is not None:
+            # loop over the preprocessors and apply each to
+            # the image
+            for p in self.preprocessors:
+                image = p.preprocess(image)
+        return np.array(image)
+
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-d", "--dataset", required=True,
+	help="path to input dataset")
+args = vars(ap.parse_args())
+
+# grab the list of images that we'll be describing
+print("[INFO] loading images...")
+imagePaths = list(paths.list_images(args["dataset"]))
+# initialize the image preprocessor, load the dataset from disk,
+# and reshape the data matrix
+sp = SimplePreprocessor(32, 32)
+
+sdl = SimpleDatasetLoader(preprocessors=[sp])
+(data, labels) = sdl.load(imagePaths, verbose=500)
+data = data.reshape((data.shape[0], 3072))
+
+# create a class for testing and sort by name
+classes = []
+for label in labels:
+    classes.append(label)
+classes = list(set(classes))
+classes.sort()
+
+# encode the labels as integers
+le = LabelEncoder()
+labels = le.fit_transform(labels)
+# partition the data into training and testing splits using 75% of
+# the data for training and the remaining 25% for testing
+(trainX, testX, trainY, testY) = train_test_split(data, labels,
+	test_size=0.25, random_state=42)
+
+# train and evaluate a k-NN classifier on the raw pixel intensities
+print("[INFO] evaluating k-NN classifier...")
+model = KNeighborsClassifier(n_neighbors=6,
+	n_jobs=-1)
+model.fit(trainX, trainY)
+print(classification_report(testY, model.predict(testX), target_names=le.classes_))
+
+# test new image
+img_test = input("Image path?")
+
+sp = SimplePreprocessor(32, 32)
+sdl = SimpleDatasetLoader(preprocessors=[sp])
+data = sdl.load_data(img_test)
+data_arr = data.reshape((1, 3072))
+new_test=model.predict(data_arr)
+print(classes[new_test[0]])
+
